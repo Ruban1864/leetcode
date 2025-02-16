95dd6a8a2cc1cc7efbefaa57181fbc6abd647242
Ruban1864 <rubanas2005@gmail.com>
2025-02-16 13:42:48 +0530

    Time: 19 ms (60.50%) | Memory: 57.2 MB (53.66%) - LeetSync

--- 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
+++ 1798-max-number-of-k-sum-pairs/max-number-of-k-sum-pairs.java
@@ -1,18 +1,22 @@
 class Solution {
    public int maxOperations(int[] nums, int k) {
-        HashMap<Integer, Integer> map = new HashMap<>();  // Stores frequency of each number
-        int count = 0;  // Keeps track of the number of valid pairs
+      Arrays.sort(nums);
+      int i=0 , j=nums.length-1 , count=0;
 
-        for (int num : nums) {  // Iterate through each number in the array
-            int complement = k - num;  // Find the complement that forms sum k
-
-            if (map.getOrDefault(complement, 0) > 0) {  // If complement exists in map
-                count++;  // A valid pair is found
-                map.put(complement, map.get(complement) - 1);  // Decrease frequency of complement
-            } else {
-                map.put(num, map.getOrDefault(num, 0) + 1);  // Store num in map
-            }
+      while(i<j){
+        if(nums[i]+nums[j] < k){
+            i++;
         }
-        return count;  // Return total valid pairs
-    }
+        else if(nums[i]+nums[j] >  k){
+            j--;
+        }
+        else{
+            count++;
+            i++;
+            j--;
+        }
+    
+      }return count;
+   }
 }
+         
