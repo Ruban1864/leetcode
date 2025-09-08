cefd6238a25c1f8a12536a45ffcce5bbeefc81a6
Ruban A S <rubanas2005@gmail.com>
2025-09-08 18:50:32 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (25.84%) - LeetSync

--- 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
+++ 1440-convert-integer-to-the-sum-of-two-no-zero-integers/convert-integer-to-the-sum-of-two-no-zero-integers.java
@@ -1,20 +1,18 @@
 class Solution {
     public int[] getNoZeroIntegers(int n) {
-        int[] arr ;
-        int a = 1 , b = n - 1;
-        for(int i  = 1 ; i < n ; i++){
-             if( !isNonZero(a) || !isNonZero(b)){
-                a++;
-                 b--;
-            }
+        int a = 1, b = n - 1;
+        while (!isNonZero(a) || !isNonZero(b)) {
+            a++;
+            b--;
         }
-       
-        return new int[]{a,b};
+        
+        return new int[]{a, b};
     }
-    private boolean isNonZero(int n){
-        while( n > 0){
-            if( n % 10 == 0) return false;
-            n /= 10 ;
+
+    private boolean isNonZero(int n) {
+        while (n > 0) {
+            if (n % 10 == 0) return false;  
+            n /= 10;
         }
         return true;
     }
